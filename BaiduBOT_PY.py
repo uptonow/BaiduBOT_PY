852ab7f7c5cc6dd80d8d8eaec91b488ea4b30798
EagLB <380388472@qq.com>
2018-07-16 16:44:33 +0800

    update getresponse function, add chooseoption, update test function choose option is temperarily unavailable

--- BaiduBOT_PY.py
+++ BaiduBOT_PY.py
@@ -11,25 +11,25 @@ import random
 
 #API_KEY and SEVRET_KEY are authorized from Baidu.
 
-def show_choose_confidence(action_list):
-    confidence_list = []
-    #show all response and confidence
-    for single_action in action_list:
-        #confidence_list.append(single_action['confidence'])
-        print(single_action['say'],'\n',single_action['confidence'])
+{
+#def show_choose_confidence(action_list):
+#    #confidence_list = []
+#    ##show all response and confidence
+#    #for single_action in action_list:
+#    #    #confidence_list.append(single_action['confidence'])
+#    #    print(single_action['say'],single_action['confidence'])
 
-    ##choose confidence for test
-    #print('Choose one response(1~%d):' %(len(action_list)),end = ' ')
-    #while True:
-    #    try:
-    #        choose_confidence = int(input())
-    #        if (choose_confidence>=1 and choose_confidence<=len(action_list)): 
-    #            return action_list[choose_confidence-1]
-    #        else: print('please input in the range(1~%d): ' %len(action_list),end = ' ')   #false handle
-    #    except ValueError: print('please input integer: ',end = ' ')                       #false handle
-
-    #max confidence
-    return  action_list[0]  
+#    ##choose confidence for test
+#    #print('Choose one response(1~%d):' %(len(action_list)),end = ' ')
+#    #while True:
+#    #    try:
+#    #        choose_confidence = int(input())
+#    #        if (choose_confidence>=1 and choose_confidence<=len(action_list)): 
+#    #            return action_list[choose_confidence-1]
+#    #        else: print('please input in the range(1~%d): ' %len(action_list),end = ' ')   #false handle
+#    #    except ValueError: print('please input integer: ',end = ' ')                       #false handle
+#    return  action_list[0]  #max confidence
+}#show_choose_confidence
 
 def get_access_token():
     #host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=API_KEY&client_secret=SECRET_KEY'
@@ -43,9 +43,9 @@ def get_access_token():
         print('Get access_token successfully')
         return access_token
 
-def get_bot_response(str_access_token, ask_data, botsession):
+def get_bot_response(str_access_token, ask_data, type, botsession):
     url = 'https://aip.baidubce.com/rpc/2.0/unit/bot/chat?access_token=' + str_access_token
-    print(botsession)
+    #print(botsession)
     post_data = {
         "version": "2.0",
         "bot_id":"7411",
@@ -56,7 +56,7 @@ def get_bot_response(str_access_token, ask_data, botsession):
             "bernard_level":'1',
             "user_id":"1",
             "query_info":{
-                "type": "TEXT",
+                "type": type,
                 "source":"KEYBOARD"},
             "query": ask_data}
         }
@@ -66,22 +66,53 @@ def get_bot_response(str_access_token, ask_data, botsession):
     req = request.Request(url, data = encoded_data, headers = headers)
     with request.urlopen(req) as response:
         result = json.loads(response.read().decode('utf-8'))
+    print(result)
 
-    action = result['result']['response']['action_list']
-    realaction = show_choose_confidence(action)
-    print('\n小蜜:', realaction['say'],'confidence:',realaction['confidence'],'type:',realaction['type'] )  
+    #show bot response
+    action = result['result']['response']['action_list'][0]
+    #realaction = show_choose_confidence(action)
+    print('\n小蜜:', action['say'],'confidence:',action['confidence'],'type:',action['type'])
     return result
 
+#choose_option is not available now/waiting for baidu update
+def choose_option(result):
+    #show and choose the option if exist
+    option_list = result['result']['response']['action_list'][0]['refine_detail']['option_list']
+    interaction_id = result['result']['interaction_id']
+    if(option_list != []):
+        num=0
+        for option in option_list:
+            num+=1
+            print(num,option['option'])
+        while True:
+            try:
+                option_result =int(input('Choose your answer(1~%d): ' %(len(option_list))))
+                if option_result >= 1 and option_result <= len(option_list):
+                    break
+                else: print('please input in the range(1~%d): ' %len(option_list),end = ' ')   #false handle
+            except ValueError: print('please input integer: ',end = ' ')                       #false handle
+        return json.dumps({'event_name': 'CHOICE',
+                           'interaction_id': interaction_id, 
+                           'index': str(option_result)})
+    else:
+        return ""
 
 if __name__ == '__main__':
     access_token = get_access_token()
     lastbotsession = ""
+    #option = ""
     while True:
+        #if option == "":
         print('\n输入q退出',end = ' ')
         data = input('User: ')
+        data_type = "TEXT"
+        #else:
+        #    data = option
+        #    data_type = "EVENT"
         if data == 'q': break
-        response = get_bot_response(access_token, data,lastbotsession)
+        response = get_bot_response(access_token, data,data_type,lastbotsession)
         lastbotsession = response['result']['bot_session']
+        #option = choose_option(response)
 
 
 
